4cc77f5025ba8d808608eec0ff8888999610343a
Taylor Gordon <taylorgordon20@gmail.com>
2020-03-17 21:47:55 -0700

    Unbreak CUDA

--- src/happy_gpu.cu
+++ src/happy_gpu.cu
@@ -1,10 +1,11 @@
+#include <limits>
 #include <vector>
 
 #include "errors.hpp"
 
 namespace happy {
 
-__global__ void add_kernel(int* a, int* b, int* c, int n) {
+__global__ void add_kernel(int* a, int* b, int* c, size_t n) {
   int i = blockIdx.x * blockDim.x + threadIdx.x;
   if (i < n) {
     c[i] = a[i] + b[i];
@@ -14,13 +15,14 @@ __global__ void add_kernel(int* a, int* b, int* c, int n) {
 std::vector<int> add_gpu(
     const std::vector<int>& v1, const std::vector<int>& v2) {
   ARGUMENT_CHECK(v1.size() == v2.size());
+  ARGUMENT_CHECK(v1.size() <= std::numeric_limits<unsigned int>::max());
 
   int* d_a = nullptr;
   int* d_b = nullptr;
   int* d_c = nullptr;
 
-  size_t n = v1.size();
-  size_t alloc_size = sizeof(int) * n;
+  auto n = v1.size();
+  auto alloc_size = sizeof(int) * n;
   std::vector<int> ret(n);
 
   cudaMalloc(&d_a, alloc_size);
@@ -30,8 +32,8 @@ std::vector<int> add_gpu(
   cudaMemcpy(d_a, v1.data(), alloc_size, cudaMemcpyHostToDevice);
   cudaMemcpy(d_b, v2.data(), alloc_size, cudaMemcpyHostToDevice);
 
-  constexpr size_t kBlockSize = 256;
-  size_t grid_size = 1 + (n - 1) / kBlockSize;
+  auto kBlockSize = 256u;
+  auto grid_size = static_cast<unsigned int>(1 + (n - 1) / kBlockSize);
   add_kernel<<<grid_size, kBlockSize>>>(d_a, d_b, d_c, n);
 
   cudaMemcpy(ret.data(), d_c, alloc_size, cudaMemcpyDeviceToHost);
